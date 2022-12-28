c4c2f5b5b57486806d4e0b7233beb0c08b21a224
Tarick <gabre.tarick@gmail.com>
2022-12-28 16:00:09 -0300

    TDD p atingir 100% d cobertura total e 100% da função getSavedCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,19 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Ao executar a função, o método solicitado é chamado', () => {
+    expect.assertions(1);
+
+    getSavedCartItems();
+
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('Ao executar a função, o método solicitado é chamado com parâmetro requisito', () => {
+    expect.assertions(1);
+
+    getSavedCartItems();
+    
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems')
+  });
 });
