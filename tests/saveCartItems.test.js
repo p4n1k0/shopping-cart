9fd65737ed531d85888f7df9b19c2f1f4c26ebc2
Tarick <gabre.tarick@gmail.com>
2022-12-28 15:23:54 -0300

    TDD d no mínimo 75% d cobertura total e 100% da função saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,11 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Ao executar a função com um parâmetro definido, o método previsto é chamado com dois parâmentros, sendo eles o cartItems e o valor passado como parâmentro da funçao saveCartItems', () => {
+    expect.assertions(1);
+
+    saveCartItems('<ol><li>Item</li></ol>');
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
