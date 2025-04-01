63da105dd12bdb6f1696045b9eed46526402c1c8
p4n1k0 <gabre.tarick@gmail.com>
2025-03-31 22:18:47 -0300

    refatorando

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,8 +1,7 @@
 const fetchItem = async (id) => {
   try {
-    if (!id) {
-      throw new Error('You must provide an url');
-    } else {
+    if (!id) throw new Error('You must provide an url');
+    else {
       const endpoint = await fetch(`https://api.mercadolibre.com/items/${id}`);
       const data = await endpoint.json();
 
--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,11 +1,9 @@
 const fetchProducts = async (product) => {
   try {
-    if (!product) {
-      throw new Error('You must provide an url');
-    } else {
+    if (!product) throw new Error('You must provide an url');
+    else {
       const endpoint = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
       const data = await endpoint.json();
-
       return data;
     }
   } catch (err) {
